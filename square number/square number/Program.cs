945b1ee18002bbfefc92411622e5cfcd124bf936
wantedcookieNod <111057867+wantedcookieNod@users.noreply.github.com>
2022-09-21 16:46:34 +0430

    Add files via upload

---
+++ square number/square number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            int a,b;
+            Console.WriteLine("please enter a number :");
+            a = Convert.ToInt16(Console.ReadLine());
+            b = (a * a);
+            Console.WriteLine("square of number = " + b);
+            Console.WriteLine("thank you for using this program");
+
+        }
+    }
+}
